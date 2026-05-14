f74a4f4dd0b904d992219e15826f4ab8db3b71e8
Diana0604 <diana.vallverdu@gmail.com>
2026-05-14 11:34:51 +0100

    print message and serve not loclhost

--- serve.py
+++ serve.py
@@ -9,11 +9,12 @@ from websockets.asyncio.server import serve
 
 async def echo(websocket):
     async for message in websocket:
+        print(message)
         await websocket.send("Received!")
 
 
 async def main():
-    async with serve(echo, "localhost", 8765) as server:
+    async with serve(echo, "", 8765) as server:
         await server.serve_forever()
 
 
